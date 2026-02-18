34b10f0bdd22d440386dfffa0b3d8db8cc76de1f
m <m>
2026-02-18 09:17:34 +0500

    blokladim

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,36 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+// import type { Metadata } from 'next'
+// import { GeistSans } from 'geist/font/sans'
+// import { GeistMono } from 'geist/font/mono'
+// import { Analytics } from '@vercel/analytics/next'
+// import './globals.css'
 
-export const metadata: Metadata = {
-  title: 'English Word Trainer',
-  icons: {
-    icon: '/icon.png',
-    apple: '/icon.png',
-    other: [
-      {
-        rel: 'mask-icon',
-        url: '/icon.png',
-        color: '#000000',
-      },
-    ],
-  },
-  generator: 'shohjahon',
-}
+// export const metadata: Metadata = {
+//   title: 'English Word Trainer',
+//   icons: {
+//     icon: '/icon.png',
+//     apple: '/icon.png',
+//     other: [
+//       {
+//         rel: 'mask-icon',
+//         url: '/icon.png',
+//         color: '#000000',
+//       },
+//     ],
+//   },
+//   generator: 'shohjahon',
+// }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return (
-    <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  )
-}
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+//         {children}
+//         <Analytics />
+//       </body>
+//     </html>
+//   )
+// }
